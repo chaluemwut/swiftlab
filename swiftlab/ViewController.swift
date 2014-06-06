32a1c217332f1e1c2e5ba841fb90774aa6494d88
Computer Engineering <ECP@computer-engineerings-imac91.local>
2014-06-06 14:08:52 +0700

    Initial Commit

---
+++ swiftlab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftlab
+//
+//  Created by Computer Engineering on 6/6/2557 BE.
+//  Copyright (c) 2557 nsc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swiftlabTests/swiftlabTests.swift
@@ -0,0 +1,35 @@
+//
+//  swiftlabTests.swift
+//  swiftlabTests
+//
+//  Created by Computer Engineering on 6/6/2557 BE.
+//  Copyright (c) 2557 nsc. All rights reserved.
+//
+
+import XCTest
+
+class swiftlabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
